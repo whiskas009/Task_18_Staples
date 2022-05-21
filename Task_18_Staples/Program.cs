ba4993f1a2dc53472fc255a6c291f3ac3a068098
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-21 21:55:39 +0300

    #1

---
+++ Task_18_Staples/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_18_Staples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите пожаулйста скобки: ");
+            string sequenceStaples = Console.ReadLine();
+            int sumForCheck = 0;
+            int depthSequenceLeft = 0;
+            int depthSequenceRight = 0;
+
+            foreach (char symbolStaples in sequenceStaples)
+            {
+                if (symbolStaples == '(')
+                    sumForCheck++;
+                else if (symbolStaples == ')')
+                    sumForCheck--;
+
+                if (sumForCheck < 0)
+                    break;
+            }
+            
+            if (sumForCheck != 0)
+            {
+                Console.WriteLine("Строка некорректна");
+                    return;
+            } 
+
+            for (int i = 0; i < sequenceStaples.Length; i++)
+            {
+                if (sequenceStaples[i] == '(')
+                    depthSequenceLeft++;
+                else if (sequenceStaples[i] == ')')
+                    break;
+            }
+
+            for (int i = sequenceStaples.Length - 1; i > 0; i--)
+            {
+                if (sequenceStaples[i] == ')')
+                    depthSequenceRight++;
+                else if (sequenceStaples[i] == '(')
+                    break;
+            }
+
+            if (depthSequenceLeft >= depthSequenceRight)
+                Console.WriteLine("Максимальная глубина вхождения: " + depthSequenceLeft);
+            else
+                Console.WriteLine("Максимальная глубина вхождения: " + depthSequenceRight);
+
+        }
+    }
+}
+
+
+
